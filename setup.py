668edf4d4175f9cf61e2055a34b249ed5bcb98fb
Adam Tauber <asciimoo@gmail.com>
2014-04-16 22:23:50 +0200

    [enh] setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ds',
+    version = '0.0.1',
+    author = 'Adam Tauber',
+    author_email = 'asciimoo@gmail.com',
+    description = ('Simple data query language'),
+    license = 'AGPLv3+',
+    keywords = "data query",
+    url = 'https://github.com/asciimoo/ds',
+    scripts = ['ds.py'],
+    py_modules = ['ds'],
+    packages = find_packages(),
+    install_requires = [],
+    download_url = 'https://github.com/asciimoo/dataquery/tarball/master',
+    entry_points={
+        "console_scripts": ["ds=ds:__main__"]
+    },
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU Affero General Public License v3'
+    ],
+)
